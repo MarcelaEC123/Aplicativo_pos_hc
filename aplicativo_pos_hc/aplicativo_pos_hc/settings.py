8bb8f659c08e0001fd2bf3c36efadec088d48189
MarcelaEC123 <126629842+MarcelaEC123@users.noreply.github.com>
2024-02-19 21:43:45 -0500

    ....

--- aplicativo_pos_hc/aplicativo_pos_hc/settings.py
+++ aplicativo_pos_hc/aplicativo_pos_hc/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 }
 ## MARCELA ESPITIA CUERVIO 
 #pruebas
+## marcela espiti cuervier prueba 2
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
